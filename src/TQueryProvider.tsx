6578a84893beb4d4445730688b2db9badd4f9bc9
jutrong <leek71358@gmail.com>
2024-07-22 10:17:11 +0900

    Chore: tanstack-query 패키지 설치 및 provider setting

---
+++ src/TQueryProvider.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import React, { useState } from "react";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+function TQueryProvider({ children }: Props) {
+  const [client] = useState(
+    new QueryClient({
+      defaultOptions: {
+        queries: {
+
+        },
+      },
+    })
+  );
+
+  return (
+    <QueryClientProvider client={client}>
+      {children}
+    </QueryClientProvider>
+  );
+}
+
+export default TQueryProvider;
