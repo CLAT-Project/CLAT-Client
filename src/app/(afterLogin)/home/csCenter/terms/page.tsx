9908c3efd45af874a5348327272d87dd99f403e3
shark <dydtjruu@gmail.com>
2024-09-04 11:39:21 +0900

    고객센터 기본화면 추가

---
+++ src/app/(afterLogin)/home/csCenter/terms/page.tsx
@@ -0,0 +1,32 @@
+'use client';
+
+import { useState } from 'react';
+import NavigationBar from '@/components/home/navigationBar';
+
+const termsOfServicePage = () => {
+  return (
+    <div className="flex h-screen space-x-4">
+     
+      <NavigationBar />
+
+      <main className="flex-1 p-6 h-4/5 border border-black rounded-lg overflow-y-auto">
+        <h1 className="text-2xl font-bold p-4 mb-8 border-b-2 border-7B7B7B  ">서비스 이용 약관</h1>
+
+        <h3 className='font-bold'>제 1조 (목적)</h3>
+        <div className="space-y-4">
+          <p>
+          국군의 조직과 편성은 법률로 정한다. 국가유공자·상이군경 및 전몰군경의 유가족은 법률이 정하는 바에 의하여 우선적으로 근로의 기회를 부여받는다. 대법원에 대법관을 둔다. 다만, 법률이 정하는 바에 의하여 대법관이 아닌 법관을 둘 수 있다. 근로조건의 기준은 인간의 존엄성을 보장하도록 법률로 정한다.
+          </p>
+          &nbsp;
+          <p>
+          국가안전보장에 관련되는 대외정책·군사정책과 국내정책의 수립에 관하여 국무회의의 심의에 앞서 대통령의 자문에 응하기 위하여 국가안전보장회의를 둔다. 농업생산성의 제고와 농지의 합리적인 이용을 위하거나 불가피한 사정으로 발생하는 농지의 임대차와 위탁경영은 법률이 정하는 바에 의하여 인정된다.
+          </p>
+
+        </div>
+      </main>
+
+    </div>
+  );
+};
+
+export default termsOfServicePage;
