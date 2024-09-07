dc4a37afbe3c9a401b2c613e8c16650f38f846f1
jutrong <leek71358@gmail.com>
2024-09-07 18:26:32 +0900

    Feat: sidebar className, link home

--- src/components/chat/ChatSidebar/index.tsx
+++ src/components/chat/ChatSidebar/index.tsx
@@ -1,4 +1,6 @@
+import { useUserClassQuery } from '@/hooks/queries/useUserQuery'
 import Image from 'next/image'
+import { useParams, useRouter } from 'next/navigation'
 
 const ClASSLIST = [
   {
@@ -24,11 +26,18 @@ const ClASSLIST = [
 ]
 // TODO : 데이터 형식에 맞게 수정
 const ChatSidebar = () => {
+  const { data: userClassData } = useUserClassQuery()
+  const params = useParams()
+  const router = useRouter()
+  // eslint-disable-next-line eqeqeq
+  const courseName = userClassData?.find((classItem) => classItem.chatRoomId == params.slug)?.courseName
   return (
-    <div className="h-screen w-[315px] bg-[#A1B5DC]">
-      <div className="flex justify-center py-[31px]">
+    <div className="h-screen w-[315px] bg-[#A1B5DC]  flex flex-col justify-between">
+      <div className="flex justify-center py-[31px] ">
         <div>
-          <div className="mb-[20px] h-[45px] w-[234px] rounded-18 bg-primary" />
+          <div className="mb-[20px] h-[45px] w-[234px] rounded-18 bg-primary flex justify-center items-center" >
+            <p className='text-white  text-lg font-bold'>{courseName}</p>
+          </div>
           <div className="flex flex-col items-end gap-[13px] pr-2">
             {ClASSLIST.map((item, index) => {
               return (
@@ -50,6 +59,10 @@ const ChatSidebar = () => {
             })}
           </div>
         </div>
+      </div>
+      <div className='flex items-center gap-[14px] p-[30px] cursor-pointer' onClick={() => router.push('/home')}>
+        <Image src="/images/svg/prev.svg" alt="prev" width={15} height={13} />
+        <p>홈으로 가기</p>
       </div>
     </div>
   )
