0cf38be8642646274c9248db15365ddc1cdaa19e
jutrong <leek71358@gmail.com>
2024-09-22 21:53:56 +0900

    Feat: 선택된 수업 주차 표시

--- src/components/chat/ChatSidebar/index.tsx
+++ src/components/chat/ChatSidebar/index.tsx
@@ -24,15 +24,22 @@ const ChatSidebar = () => {
     <div className="h-screen w-[315px] bg-[#A1B5DC]  flex flex-col justify-between">
       <div className="flex justify-center py-[31px] ">
         <div>
-          <div className="mb-[20px] h-[45px] w-[234px] rounded-18 bg-primary flex justify-center items-center" >
+          <div className="mb-[20px] h-[45px] w-[234px] rounded-18 bg-primary flex justify-center items-center z-10" >
             <p className='text-white  text-lg font-bold'>{courseName}</p>
           </div>
           <div className="flex flex-col items-end gap-[13px] pr-2">
             {weekRooms?.map((item, index) => {
+              // eslint-disable-next-line eqeqeq
+              const isActive = item.roomId == params.slug
               return (
-                <div className="relative flex h-[32px] w-[187px] items-center justify-center rounded-18 bg-white cursor-pointer" key={item.roomId} onClick={() => handleWeekClick(item.roomId)}>
-                  <p className="text-sm">{item.week}주차</p>
-                  <div className="absolute -left-[10%] -top-1/2 translate-y-1/3">
+                <div
+                  className={`relative flex h-[32px] w-[187px] items-center justify-center rounded-18 cursor-pointer  ${isActive ? 'bg-white text-primary' : 'bg-white text-black'
+                    }`}
+                  key={item.roomId}
+                  onClick={() => handleWeekClick(item.roomId)}
+                >
+                  <p className="text-sm relative z-20">{item.week}주차</p>
+                  <div className="absolute -left-[10%] -top-1/2 translate-y-1/3 z-10">
                     <Image
                       src="/images/svg/round.svg"
                       alt="round"
@@ -40,7 +47,7 @@ const ChatSidebar = () => {
                       height={28}
                     />
                     <div
-                      className={`absolute left-0 top-0 w-[3.74px] bg-white ${index === weekRooms.length - 1 ? 'h-[10px]' : 'h-[53px]'}`}
+                      className={`absolute left-0 top-0 w-[3.74px]  bg-white z-10 ${index === weekRooms.length - 1 ? 'h-[10px]' : 'h-[53px]'}`}
                     />
                   </div>
                 </div>
