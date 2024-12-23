395b2f1e3227795477f802654563a617a641422c
shgusgh12 <https://github.com/shgusgh12>
2024-12-24 03:47:43 +0900

    fix: 유저 api Endpoint 수정

--- src/apis/user.ts
+++ src/apis/user.ts
@@ -13,7 +13,7 @@ const getUserClass = async ({ term }: { term: string }) => {
 }
 
 const getUser = async () => {
-  const response = await Api.get<IUserResponse>(`/members`)
+  const response = await Api.get<IUserResponse>(`/member`)
 
   return response.data
 }
